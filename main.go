97f340e67b4f4b9a5269ec5d83434df7afb7995c
melophe <sunlavins@gmail.com>
2026-02-03 21:54:07 +0900

    add Fyne GUI base window

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Println("go-pdf: image to PDF converter")
+	a := app.New()
+	w := a.NewWindow("go-pdf")
+
+	w.SetContent(container.NewCenter(
+		widget.NewLabel("Image to PDF Converter"),
+	))
+
+	w.Resize(fyne.NewSize(500, 400))
+	w.ShowAndRun()
 }
